73036a3b85fce83722ce2dcb2d24bda616eb6279
Ks <annieh7330@gmail.com>
2019-11-14 23:47:06 +0800

    week-2

---
+++ jscript.js
@@ -0,0 +1,14 @@
+const message = document.getElementById("message");
+const mainfooter = document.querySelector(".mainfooter");
+const boxContainer3 = document.querySelector(".box-boxContainer3");
+const boxitemB = document.querySelectorAll(".box-itemB");
+
+
+
+message.addEventListener("click", () => {
+    message.innerHTML = "Have a Good Time!";
+});
+
+mainfooter.addEventListener("click", () => {
+    mainfooter.style.color = "red";
+});   //為什麼這傢伙不動作QAQ?
