07dfb27a37543088791963c2dcee8eae8974bf5d
Ks <annieh7330@gmail.com>
2019-12-01 15:04:24 +0800

    setState modify

--- js/web.js
+++ js/web.js
@@ -1,4 +1,22 @@
 class Top extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = { 
+          isToggleOn: true,
+          display: 'none'
+    };
+    this.handleClick = this.handleClick.bind(this);
+    }
+ 
+    handleClick() {
+  console.log(this.state);
+        this.setState(prevState => ({
+          isToggleOn: !prevState.isToggleOn,
+        // 因為異步執行為了防止點擊兩次才改變所以把style={{display:this.state.isToggleOn ? 'none':'block'}}
+        // 改放置被執行的對象中
+        }))
+      }
+
     render() {
         return (
             <header className="main-header">
@@ -10,10 +28,10 @@ class Top extends React.Component {
                 <il><a href="#">&emsp;Item4</a></il>
             </ul>
             <div className="dropdown-menu"> 
-                <img src="img/icon.png" tabindex="0" onClick="return true" />
-                <ul className="dropdown-nav">
+                <img src="img/icon.png" onClick={this.handleClick} />
+                <ul className="dropdown-nav" style={{display:this.state.isToggleOn ? 'none':'block'}}>
                   <p><a href="#">Item1</a>
-                     <span className = "menuX" tabindex="0" onClick="return true"> X </span></p> 
+                    <span className = "menuX" onClick={this.handleClick}> X </span></p> 
                   <p><a href="#">Item2</a></p>
                   <p><a href="#">Item3</a></p>
                   <p><a href="#">Item4</a></p>
@@ -25,13 +43,19 @@ class Top extends React.Component {
 };
 
 class Message extends React.Component {
-    change() {
-        message.innerHTML = "Have a nice day !"
-    }
+ constructor(props) {
+    super(props);
+    this.state = {isToggleOn: true};  //屬性設定
+    this.handleClick = this.handleClick.bind(this);  //綁定這個東西、用來調用這地方的變數(isToggleOn)
+  }
+    handleClick() {   //創了一個function
+    this.setState(prevState => ({isToggleOn: false}));  //上面的屬性可以用setState來改變
+  }
+
     render() {
         return (
             <div className="container">    
-                <h2 id="message" className="message" onClick={this.change.bind()}>Welcome Message</h2>
+                <h2 id="message" className="message" onClick={this.handleClick}>{this.state.isToggleOn ? 'Welcome Message':'Have a nice day !' }</h2>
                 <h3>Section Title</h3>
             </div>
         )
@@ -39,16 +63,21 @@ class Message extends React.Component {
 };
 
 class TopBoX extends React.Component {
-    boxes() {
-        const boxitemB = document.querySelectorAll(".box-itemB"); 
-        for (var i=0; i< boxitemB.length; i++){
-            if (boxitemB[i].style.display == 'block') {
-            boxitemB[i].style.display = 'none';
-            } else {
-                boxitemB[i].style.display = 'block';
-            }
-        }
+    constructor(props) {
+        super(props);
+        this.state = { 
+          isToggleOn: true,
+          display: 'none' 
+    };
+    this.handleClick = this.handleClick.bind(this);
     }
+ 
+        handleClick() {
+          this.setState(prevState => ({
+            isToggleOn: !prevState.isToggleOn,            
+          }));
+        }
+
     render() {
         return (
             <div>
@@ -60,32 +89,15 @@ class TopBoX extends React.Component {
                  <div className="box-itemA">Content Box3</div>
                  <div className="box-itemA">Content Box4</div>
             </div>
-            <div className="mainfooter" onClick={this.boxes.bind()}>Call to Action</div>
+            <div className="mainfooter" onClick={this.handleClick}>Call to Action</div>
             <div className="box-container3">
-                <div className="box-itemB">Content Box5</div>
-                <div className="box-itemB">Content Box6</div>
+                <div className="box-itemB" style={{display:this.state.isToggleOn ? 'none':'block'}}>Content Box5</div>
+                <div className="box-itemB" style={{display:this.state.isToggleOn ? 'none':'block'}}>Content Box6</div>
             </div>
             <div className="box-container4">    
-                <div className="box-itemB">Content Box7</div>
-                <div className="box-itemB">Content Box8</div>
+                <div className="box-itemB" style={{display:this.state.isToggleOn ? 'none':'block'}}>Content Box7</div>
+                <div className="box-itemB" style={{display:this.state.isToggleOn ? 'none':'block'}}>Content Box8</div>
             </div>
-            </div>
-        )
-    }
-};
-
-class HiddenBoX extends React.Component {
-    render() {
-        return (
-            <div>
-            <div className="box-container3">
-                <div className="box-itemB">Content Box5</div>
-                <div className="box-itemB">Content Box6</div>
-            </div>
-            <div className="box-container4">    
-                <div className="box-itemB">Content Box7</div>
-                <div className="box-itemB">Content Box8</div>
-            </div>  
             </div>
         )
     }
@@ -100,8 +112,6 @@ const App =() => {
             </div>
         );
     }
-
-
 
 ReactDOM.render(
  <App />,
